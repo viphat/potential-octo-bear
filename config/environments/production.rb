05b3457f18555b0bf92916abbff09d1c167ae917
Duong Vi Phat <viphat@gmail.com>
2014-11-26 12:37:02 +0700

    Continue Fixing Font Awesome - :((

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,6 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
   config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
-
   config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
   # Generate digests for assets URLs.
   config.assets.digest = true
